7c8376fbff5871d4d178ffea65f12b81f669dbbc
bhanuc <bhanuc@vm-0.bhanuc.koding.kd.io>
2014-04-08 11:02:09 +0000

    fixed app'

--- app.js
+++ app.js
@@ -10,4 +10,4 @@ app.use(express.compress());
 // Serve up content from public directory
 app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
